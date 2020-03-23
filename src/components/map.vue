63325e87b74c7af57afa8d4dd41c75e613a3b8b9
Abel-Moremi <abelmoremiz@gmail.com>
2020-03-23 14:37:53 +0200

    feat: UI update and npm vue-leaflet

---
+++ src/components/map.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Map',
+
+    data: () => ({
+     //
+    }),
+  }
+</script>
