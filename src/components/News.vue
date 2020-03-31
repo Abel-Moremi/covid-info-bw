f8e0fc6487063bf95a676c038b197482ac29f0f4
unknown <rk.morake18@gmail.com>
2020-03-31 18:30:26 +0200

    Added Live World data widget

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,298 +1,126 @@
 <template>
     <v-container class="my-5">
+        <v-layout row>
+        <v-flex xs12>
+          <h1>World Stats</h1>
+          <p>Updated Daily</p>
+            <br>
+        </v-flex>
+
+        <v-flex xs12>
+            <v-layout row>
+
+                <v-flex xs4 class="text-center">
+                   <v-card class="mx-auto card-margin" shaped>
+                      <h2 class="display-1 bold-text">{{info.cases}}</h2>
+                      <p>Confirmed</p>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs4 class="text-center">
+                   <v-card class="mx-auto card-margin" shaped>
+                      <h2 class="display-1 bold-text">{{info.deaths}}</h2>
+                      <p>Deaths</p>
+                  </v-card>
+              </v-flex>   
+
+              <v-flex xs4 class="text-center">
+                   <v-card class="mx-auto card-margin" shaped>
+                      <h2 class="display-1 bold-text">{{info.recovered}}</h2>
+                      <p>Recovered</p>
+                  </v-card>
+              </v-flex>
+
+            </v-layout>
+        </v-flex>
+
+      </v-layout>
+
+      <br><br>
+      <!--WHO Latest Updates Section -->
       <v-layout row>
         <v-flex xs12>
           <h1>WHO Latest Articles &amp; Updates.</h1>
             <br>
         </v-flex>
-        <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Covid19 Update</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-flex>
-                <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Covid 19 Treatment</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-flex>
-                <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Africa Update</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-flex>
-      </v-layout>
-<br><br>
-            <v-layout row>
-        <v-flex xs12>
-          <h1>Ministry of Health Latest Articles &amp; Updates.</h1>
-<br>
-        </v-flex>
         
         <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Covid19 Update</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-flex>
-                <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Covid 19 Treatment</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-flex>
-                <v-flex xs12 md6 lg4>
-          <v-card
-    max-width="344"
-    class="mx-auto"
-  >
-    <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="headline">Africa Update</v-list-item-title>
-        <v-list-item-subtitle>by WHO</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-      height="194"
-    ></v-img>
-
-    <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes today.
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Read
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Bookmark
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+    
         </v-flex>
       </v-layout>
+      <!-- End WHO Latest Updates Section-->
+
+      <br><br>
+
+      <!--Local News Articles Section -->
+      <v-layout row>
+        <v-flex xs12>
+          <h1>Local News Articles &amp; Updates.</h1>
+        </v-flex>
+        
+        <v-flex xs12 md6 lg6 v-for="post in news" :key="post.post.id">
+          <newscard
+          :title="`${post.post.title.rendered}`"
+          :body="`${post.post.excerpt.rendered}`"
+          :read="`${post.post.id}`"/>
+        </v-flex>
+
+      </v-layout>
+      <!--End Local News Articles Section -->
+      
     </v-container>
 </template>
 <script>
+import newscard from '../components/NewsCard'
+
   export default {
-    data: () => ({
-      isActive: false,
-    }),
+    components: {newscard},
+    data() {
+    return {
+      news: [],
+      info: null
+    }
+  },
+    methods: {
+      created(){
+      fetch('https://yourbotswana.com/wp-json/wp/v2/posts')
+      .then((res) => res.json())
+      .then((data) => {
+        let _this = this
+
+      //  let output = '<h2 class="mb-4">Posts</h2>';
+        data.forEach(function(post){
+           _this.news.push({
+            post
+          })
+          // this.articles.push(post)
+        });
+       // document.getElementById('output').innerHTML = output;
+      })
+    },
+      getData(){
+      fetch('https://corona.lmao.ninja/all')
+      .then((res) => res.json())
+      .then((data) => {
+        let _this = this
+              _this.info = data
+          })
+    }
+    },
+    beforeMount(){
+    this.created();
+    this.getData();
+ }
   }
 </script>
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+  .card-margin{
+    padding-top:30px;
+    padding-bottom:20px;
+  }
+  .bold-text{
+    font-family: 'Anton', sans-serif;
+    font-weight:bold;
+  }
 </style>
