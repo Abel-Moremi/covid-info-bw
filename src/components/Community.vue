687c4dd92fd03c7d890559bfc329d1a7d2e364c3
unknown <rk.morake18@gmail.com>
2020-04-03 20:16:17 +0200

    added community messags page

---
+++ src/components/Community.vue
@@ -0,0 +1,27 @@
+<template>
+<v-container>
+    <popup />
+    
+    <div v-for="message in messages"
+      :key="message.id"> 
+        <communitycard :place="`${message.place}`" :message="`${message.message}`" :author="`${message.name}`" />
+    </div>
+ 
+ </v-container>
+</template>
+
+<script>
+import communitycard from '../components/CommunityCard'
+import popup from '../components/Popup'
+import { db } from '../assets/utilities/db'
+
+  export default {
+       components: {communitycard, popup},
+    data: () => ({
+        messages : []
+    }),
+    firestore: {
+    messages: db.collection('messages'),
+  },
+  }
+</script>
