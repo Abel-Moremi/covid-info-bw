c882b6b67ad1d643a885a90c09dbf86550ce9921
unknown <rk.morake18@gmail.com>
2020-04-07 11:56:42 +0200

    added new card layout for sadc news headlines

---
+++ src/components/CardSadc.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width=""
+  >
+    <v-card-text>
+      <h2 v-html="title" class="text--primary">
+        {{title}}
+      </h2>
+      <p>{{author}}</p>
+      <div v-html="excerpt" class="text--primary">
+        {{excerpt}}
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+        :href="link"
+      >
+        read more
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+      props: ['title','author', 'excerpt', 'link'],
+    data: () => ({
+      
+    }),
+  }
+</script>
