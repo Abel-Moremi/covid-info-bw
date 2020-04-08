2afce604422d393d48e9502175ba9de45aa366b9
unknown <rk.morake18@gmail.com>
2020-04-08 23:47:34 +0200

    enable db persistence

--- src/assets/utilities/db.js
+++ src/assets/utilities/db.js
@@ -5,7 +5,24 @@ const firebaseConfig = {
     projectId: "covid-info-bw"
   };
 
+ 
+
 // Get a Firestore instance
 export const db = firebase
   .initializeApp(firebaseConfig)
   .firestore()
+
+
+  db.settings({
+    cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED
+  });
+  
+  db.enablePersistence().catch(function(err) {
+    if (err.code == 'failed-precondition') {
+      // probably multible tabs open at once
+      console.log('persistance failed');
+    } else if (err.code == 'unimplemented') {
+      // lack of browser support for the feature
+      console.log('persistance not available');
+    }
+  });
