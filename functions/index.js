90859dc0b70042eb3f6cb5c71944479e60c48c87
Abel-Moremi <abelmoremiz@gmail.com>
2020-04-12 17:55:58 +0200

    feat: Established firestore connection in function
    -  npm i @google-cloud/firestore
    -  npm i @firebase/firestore
    -  npm i @firebase/app
    - export.getData function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
+const firebaseConfig = {
+    projectId: "covid-info-bw"
+  };
+
+const db = firebase
+  .initializeApp(firebaseConfig)
+  .firestore()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase! Dawg I am beast");
+});
+
+
+exports.getData = functions.https.onRequest((req, res) => {
+    const docRef = db.collection('FunctionTest').doc('Cm38kBYShNnyuLVpizcy');
+    const getDoc = docRef.get()
+        .then(doc => {
+        if (!doc.exists) {
+            console.log('No such document!');
+            return res.send('Not Found')
+        } 
+            console.log(doc.data());
+            return res.send(doc.data());
+        })
+        .catch(err => {
+        console.log('Error getting document', err);
+    });
+});
+
+
+ 
