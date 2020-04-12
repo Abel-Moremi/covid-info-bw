8f7de376784b3991fb817c74be21b3dd2b1234ee
unknown <rk.morake18@gmail.com>
2020-04-13 01:18:32 +0200

    created ne live radio comp

---
+++ src/components/RadioLive.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container style="height: 400px;">
+      <div id="117171632"></div>
+      <v-row
+        class="fill-height"
+        align-content="center"
+        justify="center"
+      >
+        <v-col
+          class="subtitle-1 text-center"
+          cols="12"
+        >
+        <v-icon class="display-3">mdi-radio-tower</v-icon><br>
+          <strong>Live Stream Under Construction</strong><br>
+          <small>we will notify you when it is ready</small>
+        </v-col>
+        <v-col cols="6">
+          <v-progress-linear
+            color="red accent-4"
+            indeterminate
+            rounded
+            height="6"
+          ></v-progress-linear>
+          
+        </v-col>
+                <v-col
+          class="subtitle-1 text-center"
+          cols="12"
+        >
+        <v-btn href="/live" class="deep-purple white--text">watch live stream</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    methods : {
+      ad(){
+          window._mNHandle.queue.push(function (){
+              window._mNDetails.loadTag("117171632", "970x90", "117171632");
+          });
+      },
+    },
+    beforeMount(){
+        this.ad()
+    }
+  }
+</script>
