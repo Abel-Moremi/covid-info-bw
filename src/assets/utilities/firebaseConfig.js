eae4010852b4296cf6abd7da3b105abc485fb563
Abel-Moremi <abelmoremiz@gmail.com>
2020-04-27 20:17:54 +0200

    fix: add .env variables for firebaseConfig

--- src/assets/utilities/firebaseConfig.js
+++ src/assets/utilities/firebaseConfig.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-    projectId: "covid-info-bw",
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
